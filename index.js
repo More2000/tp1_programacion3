21ac7ad61cb1f1ab371aba87769cf3efe3529515
More2000 <francomorellato00@gmail.com>
2024-06-25 19:14:11 -0300

    segundo commit

--- index.js
+++ index.js
@@ -71,6 +71,7 @@ var options = {
   explorer: true
 };
 app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerDocument,options))
+
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`)
 })
